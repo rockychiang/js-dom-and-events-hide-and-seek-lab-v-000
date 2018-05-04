90effcb530751a892271f46ea54209c1a68945af
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-04 03:50:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("div#nested div div div div.target")
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll("main#app ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector("div#grand-node div div div div")
+}
